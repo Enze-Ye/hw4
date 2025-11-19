16346cbb45a2ca4a723ea72f157005754bed684a
Enze-Ye <ye000242@umn.edu>
2025-11-19 15:15:05 +0800

    code change

--- sq_reduction.py
+++ sq_reduction.py
@@ -2,25 +2,26 @@
 
 import subprocess
 import math
+import random
+
+SQ_PATH = "/project/web-classes/Fall-2025/csci5471/hw4/sq"
 
 def read_modulus():
-    out = subprocess.check_output(["sq", "modulus"])
+    out = subprocess.check_output([SQ_PATH, "modulus"])
     return int(out.strip(), 16)
 
 def der_encode(x):
-    # Encode as DER INTEGER
-    b = x.to_bytes((x.bit_length()+7)//8, "big")
+    b = x.to_bytes((x.bit_length() + 7) // 8, "big")
     if b[0] >= 0x80:
         b = b"\x00" + b
-    return b"\x02" + len(b).to_bytes(1,"big") + b
+    return b"\x02" + len(b).to_bytes(1, "big") + b
 
 def run_sqrt(a_bytes):
-    out = subprocess.check_output(["sq", "sqrt"], input=a_bytes)
+    out = subprocess.check_output([SQ_PATH, "sqrt"], input=a_bytes)
     return int(out.strip(), 16)
 
 def trial(n):
-    import random
-    x = random.randrange(2, n-1)
+    x = random.randrange(2, n - 1)
     a = pow(x, 2, n)
     a_der = der_encode(a)
 
@@ -29,7 +30,7 @@ def trial(n):
     except:
         return None
 
-    if y == x or y == n-x:
+    if y == x or y == n - x:
         return None
 
     g = math.gcd(x - y, n)
